606e1b9a99e4a84bcdcacab7b4200a1943ce68b1
zhang.rog <bankzhy@gmail.com>
2025-02-02 21:51:22 +0900

    init

--- clone/run_graphbert.sh
+++ clone/run_graphbert.sh
@@ -1,5 +1,5 @@
 mkdir saved_models
-python run_graphbert.py \
+nohup python run_graphbert.py \
     --output_dir=saved_models \
     --config_name=microsoft/graphcodebert-base \
     --model_name_or_path=microsoft/graphcodebert-base \
@@ -8,12 +8,12 @@ python run_graphbert.py \
     --train_data_file=dataset/dataset/train.txt \
     --eval_data_file=dataset/dataset/valid.txt \
     --test_data_file=dataset/dataset/test.txt \
-    --epoch 1 \
+    --epoch 5 \
     --code_length 512 \
     --data_flow_length 128 \
-    --train_batch_size 4 \
-    --eval_batch_size 4 \
+    --train_batch_size 16 \
+    --eval_batch_size 16 \
     --learning_rate 2e-5 \
     --max_grad_norm 1.0 \
     --evaluate_during_training \
-    --seed 123456 2>&1| tee saved_models/train.log
+    --seed 123456 2>&1| tee saved_models/train.log > output.log 2>&1 &
