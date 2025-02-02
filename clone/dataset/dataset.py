e25a34a578668db78bba2c6051313f36776063af
zhang.rog <bankzhy@gmail.com>
2025-02-02 17:48:04 +0900

    init

--- clone/dataset/dataset.py
+++ clone/dataset/dataset.py
@@ -116,35 +116,34 @@ def extract_dataflow(code, parser,lang):
     #obtain dataflow
     if lang=="php":
         code="<?php"+code+"?>"
-    # try:
-    print(parser)
-    tree = parser[0].parse(bytes(code,'utf8'))
-    root_node = tree.root_node
-    tokens_index=tree_to_token_index(root_node)
-    code=code.split('\n')
-    code_tokens=[index_to_code_token(x,code) for x in tokens_index]
-    index_to_code={}
-    for idx,(index,code) in enumerate(zip(tokens_index,code_tokens)):
-        index_to_code[index]=(idx,code)
     try:
-        DFG,_=parser[1](root_node,index_to_code,{})
-    except:
-        DFG=[]
-    DFG=sorted(DFG,key=lambda x:x[1])
-    indexs=set()
-    for d in DFG:
-        if len(d[-1])!=0:
-            indexs.add(d[1])
-        for x in d[-1]:
-            indexs.add(x)
-    new_DFG=[]
-    for d in DFG:
-        if d[1] in indexs:
-            new_DFG.append(d)
-    dfg=new_DFG
-    # except Exception as e:
-    #     print(e)
-    #     dfg=[]
+        tree = parser[0].parse(bytes(code,'utf8'))
+        root_node = tree.root_node
+        tokens_index=tree_to_token_index(root_node)
+        code=code.split('\n')
+        code_tokens=[index_to_code_token(x,code) for x in tokens_index]
+        index_to_code={}
+        for idx,(index,code) in enumerate(zip(tokens_index,code_tokens)):
+            index_to_code[index]=(idx,code)
+        try:
+            DFG,_=parser[1](root_node,index_to_code,{})
+        except:
+            DFG=[]
+        DFG=sorted(DFG,key=lambda x:x[1])
+        indexs=set()
+        for d in DFG:
+            if len(d[-1])!=0:
+                indexs.add(d[1])
+            for x in d[-1]:
+                indexs.add(x)
+        new_DFG=[]
+        for d in DFG:
+            if d[1] in indexs:
+                new_DFG.append(d)
+        dfg=new_DFG
+    except Exception as e:
+        print(e)
+        dfg=[]
     return code_tokens,dfg
 
 
