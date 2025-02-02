75a36c0162729864c60081cfd2f1848749aded28
zhang.rog <bankzhy@gmail.com>
2025-02-02 17:36:10 +0900

    init

--- clone/dataset/dataset.py
+++ clone/dataset/dataset.py
@@ -117,30 +117,30 @@ def extract_dataflow(code, parser,lang):
     if lang=="php":
         code="<?php"+code+"?>"
     # try:
-        tree = parser[0].parse(bytes(code,'utf8'))
-        root_node = tree.root_node
-        tokens_index=tree_to_token_index(root_node)
-        code=code.split('\n')
-        code_tokens=[index_to_code_token(x,code) for x in tokens_index]
-        index_to_code={}
-        for idx,(index,code) in enumerate(zip(tokens_index,code_tokens)):
-            index_to_code[index]=(idx,code)
-        try:
-            DFG,_=parser[1](root_node,index_to_code,{})
-        except:
-            DFG=[]
-        DFG=sorted(DFG,key=lambda x:x[1])
-        indexs=set()
-        for d in DFG:
-            if len(d[-1])!=0:
-                indexs.add(d[1])
-            for x in d[-1]:
-                indexs.add(x)
-        new_DFG=[]
-        for d in DFG:
-            if d[1] in indexs:
-                new_DFG.append(d)
-        dfg=new_DFG
+    tree = parser[0].parse(bytes(code,'utf8'))
+    root_node = tree.root_node
+    tokens_index=tree_to_token_index(root_node)
+    code=code.split('\n')
+    code_tokens=[index_to_code_token(x,code) for x in tokens_index]
+    index_to_code={}
+    for idx,(index,code) in enumerate(zip(tokens_index,code_tokens)):
+        index_to_code[index]=(idx,code)
+    try:
+        DFG,_=parser[1](root_node,index_to_code,{})
+    except:
+        DFG=[]
+    DFG=sorted(DFG,key=lambda x:x[1])
+    indexs=set()
+    for d in DFG:
+        if len(d[-1])!=0:
+            indexs.add(d[1])
+        for x in d[-1]:
+            indexs.add(x)
+    new_DFG=[]
+    for d in DFG:
+        if d[1] in indexs:
+            new_DFG.append(d)
+    dfg=new_DFG
     # except Exception as e:
     #     print(e)
     #     dfg=[]
