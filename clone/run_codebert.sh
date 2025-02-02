a9ac38a7c6bea575b43f5e3408fe55d3f21c8e4c
zhang.rog <bankzhy@gmail.com>
2025-02-02 16:25:08 +0900

    init

--- clone/run_codebert.sh
+++ clone/run_codebert.sh
@@ -5,14 +5,14 @@ python run_codebert.py \
     --model_name_or_path=microsoft/codebert-base \
     --tokenizer_name=microsoft/codebert-base \
     --do_train \
-    --train_data_file=dataset/dataset/small/train.txt \
-    --eval_data_file=dataset/dataset/small/valid.txt \
-    --test_data_file=dataset/dataset/small/test.txt \
-    --epoch 1 \
+    --train_data_file=dataset/dataset/train.txt \
+    --eval_data_file=dataset/dataset/valid.txt \
+    --test_data_file=dataset/dataset/test.txt \
+    --epoch 10 \
     --code_length 512 \
     --data_flow_length 128 \
-    --train_batch_size 4 \
-    --eval_batch_size 4 \
+    --train_batch_size 32 \
+    --eval_batch_size 32 \
     --learning_rate 2e-5 \
     --max_grad_norm 1.0 \
     --evaluate_during_training \
