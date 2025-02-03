fa120d2f0e62e314336b19e511cf1c0208fb7819
zhang.rog <bankzhy@gmail.com>
2025-02-03 11:17:28 +0900

    init

--- clone/models.py
+++ clone/models.py
@@ -77,19 +77,18 @@ class GraphBertModel(nn.Module):
         position_idx = torch.cat((position_idx_1.unsqueeze(1), position_idx_2.unsqueeze(1)), 1).view(bs * 2, l)
         attn_mask = torch.cat((attn_mask_1.unsqueeze(1), attn_mask_2.unsqueeze(1)), 1).view(bs * 2, l, l)
 
-        print("re input")
-        print(inputs_ids)
-        print(position_idx)
-        print(attn_mask)
-
         # embedding
         nodes_mask = position_idx.eq(0)
         token_mask = position_idx.ge(2)
         inputs_embeddings = self.encoder.roberta.embeddings.word_embeddings(inputs_ids)
+        print("inputs_embeddings")
+        print(inputs_embeddings)
         nodes_to_token_mask = nodes_mask[:, :, None] & token_mask[:, None, :] & attn_mask
         nodes_to_token_mask = nodes_to_token_mask / (nodes_to_token_mask.sum(-1) + 1e-10)[:, :, None]
         avg_embeddings = torch.einsum("abc,acd->abd", nodes_to_token_mask, inputs_embeddings)
         inputs_embeddings = inputs_embeddings * (~nodes_mask)[:, :, None] + avg_embeddings * nodes_mask[:, :, None]
+        print("inputs_embeddings2")
+        print(inputs_embeddings)
 
         outputs = \
         self.encoder.roberta(inputs_embeds=inputs_embeddings, attention_mask=attn_mask, position_ids=position_idx,
